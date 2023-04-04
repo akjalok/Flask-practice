cb75ddde57e31e88d77bcf6af73677e315682d39
akjalok <akj2947@gmail.com>
2023-04-04 18:35:17 +0530

    update

--- vid_streaming/app.py
+++ vid_streaming/app.py
@@ -2,15 +2,20 @@ from flask import Flask,render_template,Response
 import cv2
 app = Flask(__name__)
 camera = cv2.VideoCapture(0)#using the system default camera
-while True:
+
 def generate_frames():
-    while True:##to 
+    while True:
     ##read the camera frame
-        success,frame = camera.read()##the .read() method has return two values i.e. if the camera is running,the frames if the camera is running
+        success,frame = camera.read()
+        ##the .read() method has return two values i.e. if the camera is running,the frames if the camera is running
         if not success:
-            break
+                break
         else:
+            
             ret,buffer=cv2.imencode('.jpg',frame)
+            frame = buffer.tobytes()
+        yield (b'--frame \r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n'+frame+b'\r\n')
 
 @app.route('/')
 def Welcome():
